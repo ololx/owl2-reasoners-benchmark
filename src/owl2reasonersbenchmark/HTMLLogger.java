1edbe10838a33c01035d93bba18d3154b4d8c144
Alexander A. Kropotin <ololx@icloud.com>
2014-11-18 09:56:08 +0500

    init(clone this project from my other repository)

---
+++ src/owl2reasonersbenchmark/HTMLLogger.java
@@ -0,0 +1,69 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package owl2reasonersbenchmark;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author Administrator
+ */
+public class HTMLLogger {
+    private String path;
+    private FileWriter log;
+    
+    protected void finalize () throws Throwable {
+        try {
+            this.write("</table>");
+            closeLog();
+            this.log = null;
+            this.path = null;
+        } finally {
+            super.finalize();
+        }
+    }
+    
+    public HTMLLogger () {
+        
+    }
+    public HTMLLogger (String path) throws IOException {
+        setPath (path);
+        setLog (new File (this.path));
+        this.write("<table border = '0'>");
+    }
+    public void setPath (String path) {
+        this.path = path;
+    }
+    private void setLog (File file) throws IOException {
+        this.log = new FileWriter(file);
+    }
+    public void closeLog () throws IOException {
+        this.log.close ();
+    }
+    public void write (String text) throws IOException {
+        this.log.append(text);
+    }
+    public void write (String type, String text) throws IOException {
+        String color;
+        switch (type.toUpperCase()) {
+            case "INFO":
+                color = "Grey";
+                break;
+            case "ALLERT":
+                color = "Red";
+                break;
+            case "ATTENTION":
+                color = "Yellow";
+                break;
+            default:
+                color = "Grey";
+                break;
+        }
+        this.log.append("<tr bgcolor = '"+color+"'><td><b>"+text+"</b></td></tr>");
+        System.out.println (type.toUpperCase()+":"+text);
+    }
+}
